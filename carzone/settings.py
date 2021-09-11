00122c4b34f6138175131b0fca942db22be08849
ricardo-dota <58540274+ricardo-dota@users.noreply.github.com>
2021-09-11 02:39:43 -0300

    configuraciones 10

--- carzone/settings.py
+++ carzone/settings.py
@@ -19,9 +19,9 @@ from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'carzone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(__file__),'templates')] ,
+        'DIRS': ['templates'] ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
